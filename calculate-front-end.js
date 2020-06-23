8d0a1a89afb6e358d630712d39e2c21893dd3da2
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-06-23 15:05:53 -0400

    done

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -1,9 +1,15 @@
 /*********************************************
  * YOUR CODE HERE!                           *
  ********************************************/
+const calculate = require('./calculate-back-end.js')
 
+const num1 = process.argv[2];
+const operation = process.argv[3];
+const num2 = process.argv[4];
 
+console.log(calculate(num1,num2,operation))
 
+const result = calculate(num1,num2,operation)
 
 
 
