34d10bd8e41d12bec8953805e67c208189fe163c
DeAundre Prescott <deaundre.prescott@codeimmersives.com>
2020-06-23 15:08:54 -0400

    finsh

--- calculate-front-end.js
+++ calculate-front-end.js
@@ -3,15 +3,15 @@
  ********************************************/
 const calculate = require('./calculate-back-end.js')
 
-const num1 = process.argv[2];
-const operation = process.argv[3];
-const num2 = process.argv[4];
+const num1 = getInput[2];
+const operation = getInput[3];
+const num2 = getInput[4];
 
 console.log(calculate(num1,num2,operation))
 
 const result = calculate(num1,num2,operation)
 
-
+console.log(result)
 
 /******************************************
  * OUR CODE BELOW--DON'T TOUCH!           *
